cc5f212994290a8d77544f29b8c22503544c4999
Simrin Joshi <simrinjoshi@yahoo.com>
2022-04-16 12:45:30 +0530

    Create TrimBinarySearchTree.java

---
+++ TrimBinarySearchTree.java
@@ -0,0 +1,19 @@
+
+/** It is not wise idea to use iterative method because we need to traverse through right and left sub branches **/
+
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+       
+        if(root==null)
+            return null;
+        else if(root.val < low)
+            return trimBST(root.right,low,high);
+        else if(root.val > high) 
+            return trimBST(root.left,low,high);
+        else {
+            root.left = trimBST(root.left,low,high);
+            root.right = trimBST(root.right,low,high);
+            return root;
+        }
+    }
+}
