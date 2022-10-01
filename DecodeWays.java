2cf39c0dfe7394722448f60c4e86d9f9df44d6ac
Simrin Joshi <simrinjoshi@yahoo.com>
2022-10-01 11:06:48 +0530

    Create DecodeWays.java

---
+++ DecodeWays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodings(String s) {
+            int[] dp = new int[s.length()+1];
+            dp[0]= 1;
+            dp[1] = s.charAt(0)=='0'?0:1;
+
+            for(int i=2;i<=s.length();i++) {
+                int firstD = Integer.valueOf(s.substring(i-1,i));
+                int secondD = Integer.valueOf(s.substring(i-2,i));
+
+                if(firstD>=1)
+                    dp[i] = dp[i] + dp[i-1]; 
+                if(secondD >= 10 && secondD <= 26)
+                    dp[i] = dp[i] + dp[i-2];
+            }
+
+            return dp[s.length()];
+        }
+    }
